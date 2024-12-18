59ccbcdec5386f8c51a64566287e753f39ca706b
duarizwan <sz1769193@gmail.com>
2024-12-18 11:45:52 +0500

    my code girls assignment work

---
+++ 3class.js
@@ -0,0 +1,20 @@
+// console.log("what is your age ?");
+// process.stdin.on("data" , data =>{
+//     console.log(`Answer: ${data}`)
+//     process.exit();
+// })
+
+//difference between var and let
+
+// for(var a=0;a<=5;a++){
+//     setTimeout( function(){
+//         console.log(a);    
+        
+//             },8000)}
+            
+// for(let a=0;a<=5;a++){
+//     setTimeout( function(){
+// console.log(a);    
+
+//     },a*1000)
+// }
---
+++ script.js
@@ -0,0 +1,7 @@
+let abc = process.argv.slice(2)
+//console.log(abc);
+abc.forEach(abcd => {
+    abcd.split(abcd,"");
+    abcd.replace("--","");
+console.log(abcd);
+})
