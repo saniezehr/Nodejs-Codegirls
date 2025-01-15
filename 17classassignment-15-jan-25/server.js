df2df7abcf7e3f6214b96bbc8cdc594e97cf5e8c
duarizwan <sz1769193@gmail.com>
2025-01-15 12:17:39 +0500

    my assignment

---
+++ 17classassignment-15-jan-25/server.js
@@ -0,0 +1,23 @@
+// import express from 'express'
+// import path from 'path'
+const express = require('express')
+const app =express();
+const port =6789;
+const path = require('path')
+const ejs = require('ejs');
+import ejs from 'ejs'
+jjj
+
+
+
+// app.use(express.static('public'))
+// app.use('/static' , express.static(path.join(__dirname,'public')))
+
+// app.set("view engine","ejs");
+
+
+// app.get("/" , (req,res) => {
+//     res.render("index");
+// })
+
+// app.listen(port)
